62c1a7ae27970a44aaa0c9f1f72672656eba2ff4
kth0027 <so_dream@naver.com>
2022-03-25 03:58:35 +0900

    부동산 프로젝트 진행

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>
+        <!-- {{logo}} -->
+        <h4 class="red" :style="스타일">XX 원룸</h4>
+        <P>{{price1}} 만원</P>
+    </div>
+    <div>
+        <h4>XX 원룸</h4>
+        <P>{{price2}} 만원</P>
+    </div>
+    <div>
+        <h4>XX 원룸</h4>
+        <P>{{price2}} 만원</P>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            price1: 60,
+            price2: 70,
+            // logo : '원룸샵',
+            // 스타일 : 'color : blue',
+            products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+        }
+    },
+    components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
